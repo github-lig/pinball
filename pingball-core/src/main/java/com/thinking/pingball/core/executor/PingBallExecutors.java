d731acc4533686fb10493b8ece67ed59af282244
ligang25 <ligang25@58.com>
2021-10-09 19:00:12 +0800

    1

---
+++ pingball-core/src/main/java/com/thinking/pingball/core/executor/PingBallExecutors.java
@@ -0,0 +1,24 @@
+package com.thinking.pingball.core.executor;
+
+/**
+ * @Description
+ *  创建线程池
+ *      ThreadFactory
+ *      RejectedExecutionHandler
+ *      BlockingQueue
+ *  修改线程池信息
+ *      ThreadPoolExecutors一共提供了5个set方法
+ *          setCorePoolSize
+ *          setKeepAliveTime
+ *          setMaximumPoolSize
+ *          setRejectedExecutionHandler
+ *          setThreadFactory
+ *      核心线程数、线程存活时间、最大线程数有设置的必要
+ *  维护线程池引用
+ *
+ * @Author Lig
+ * @Date 2021/10/9 5:43 下午
+ */
+public class PingBallExecutors {
+
+}
